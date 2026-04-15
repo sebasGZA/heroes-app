2f6495e153dcaf67f64bd002c56e55a9fa0960c7
sebasGZA <sebassga34@gmail.com>
2026-04-14 23:04:30 -0500

    feat: first commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { Button } from '@/components/ui/button'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <h1>Heroes App</h1>
+    <Button>click me</Button>
   </StrictMode>,
 )
