8fb2fc6243083f3197786ddba9db6ff0ecc6eb27
sebasGZA <sebassga34@gmail.com>
2026-04-16 15:24:42 -0500

    feat: hero grid card components

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -6,8 +6,9 @@ function Progress({
   className,
   children,
   value,
+  activeColor = 'bg-primary',
   ...props
-}: ProgressPrimitive.Root.Props) {
+}: ProgressPrimitive.Root.Props & { activeColor?: string }) {
   return (
     <ProgressPrimitive.Root
       value={value}
@@ -17,7 +18,7 @@ function Progress({
     >
       {children}
       <ProgressTrack>
-        <ProgressIndicator />
+        <ProgressIndicator activeColor={activeColor} />
       </ProgressTrack>
     </ProgressPrimitive.Root>
   )
@@ -38,12 +39,13 @@ function ProgressTrack({ className, ...props }: ProgressPrimitive.Track.Props) {
 
 function ProgressIndicator({
   className,
+  activeColor,
   ...props
-}: ProgressPrimitive.Indicator.Props) {
+}: ProgressPrimitive.Indicator.Props & { activeColor?: string }) {
   return (
     <ProgressPrimitive.Indicator
       data-slot="progress-indicator"
-      className={cn("h-full bg-primary transition-all", className)}
+      className={cn(`h-full transition-all ${activeColor}`, className)}
       {...props}
     />
   )
