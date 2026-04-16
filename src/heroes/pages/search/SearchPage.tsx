e69706c752292e4bb1cde748eae3d418e19973a6
sebasGZA <sebassga34@gmail.com>
2026-04-16 14:48:37 -0500

    feat: add search, hero and home bases

--- src/heroes/pages/search/SearchPage.tsx
+++ src/heroes/pages/search/SearchPage.tsx
@@ -1,8 +1,16 @@
+import { CustomJumbotron } from "@/components/custom/CustomJumbotron"
+import { HeroStats } from "@/heroes/components/HeroStats";
+
 const SearchPage = () => {
   return (
-    <div>
-      Search Page
-    </div>
+    <>
+      <CustomJumbotron
+        title="Search a Superhero"
+        description="Search your favorite superheroe or villain"
+      />
+
+      <HeroStats />
+    </>
   )
 }
 
--- src/router/app.routes.tsx
+++ src/router/app.routes.tsx
@@ -1,8 +1,8 @@
 import { createBrowserRouter } from "react-router";
 
 import { HeroesLayout } from "@/heroes/layouts/HeroesLayout";
-import { HeroPage } from "@/heroes/pages/hero/HeroPage";
-import { HomePage } from "@/heroes/pages/home/HomePage";
+import HeroPage from "@/heroes/pages/hero/HeroPage";
+import HomePage from "@/heroes/pages/home/HomePage";
 import { AdminLayout } from "@/admin/layouts/AdminLayout";
 import { lazy } from "react";
 
